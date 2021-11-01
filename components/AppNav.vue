9a73996d9bf24615484ec0d335afd6bc43e9dee3
morgan4080 <morganmurungi@live.com>
2021-11-01 03:45:55 +0300

    notaslave

--- components/AppNav.vue
+++ components/AppNav.vue
@@ -67,7 +67,7 @@
                   leave-class="transform opacity-100 translate-y-0"
                   leave-to-class="transform opacity-0 translate-y-1"
                 >
-                  <div v-if="showSubMenu" id="evOfInt"  class="absolute right-0 -mr-3 pt-5 transform px-2 w-screen lg:mt-2 sm:px-0 lg:ml-0 lg:left-1/2 capitalize" style="max-width: 15rem;" @mouseleave="showSubMenu0 = false" >
+                  <div v-if="showSubMenu" id="evOfInt"  class="absolute right-0 -mr-3 pt-5 transform px-2 w-screen lg:mt-2 sm:px-0 lg:ml-0 lg:left-2/3 capitalize" style="max-width: 15rem;" @mouseleave="showSubMenu0 = false" >
                     <div class="rounded-lg shadow-2xl">
                       <div class="rounded-lg shadow-xl">
                         <div class="rounded-lg z-20 relative grid gap-6 px-5 sm:gap-8 sm:p-6 bg-gradient-to-br from-yellow-gold to-yellow-600" >
@@ -91,87 +91,6 @@
                 Get Quote
               </nuxt-link>
 
-              <div v-show="!$auth.loggedIn" class="relative flex justify-center text-left">
-                <button class="uppercase inline-flex items-center mx-2 text-black tracking-wide font-semibold py-3 px-4 bg-white text-black rounded-full transform hover:scale-105 transition ease-in-out duration-100" @click="showDropdown" >
-                  <span>My Account</span>
-                  <svg class="-mr-1 ml-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                  </svg>
-                </button>
-                <transition
-                  enter-active-class="transition ease-out duration-200"
-                  leave-active-class="transition ease-in duration-150"
-                  enter-class="transform opacity-0 translate-y-1"
-                  enter-to-class="transform opacity-100 translate-y-0"
-                  leave-class="transform opacity-100 translate-y-0"
-                  leave-to-class="transform opacity-0 translate-y-1"
-                >
-                  <div v-show="dropDownOpen" class="absolute right-0 -mr-3 px-2 w-screen mt-16 sm:px-0 lg:ml-0 capitalize" style="max-width: 15rem;"  @mouseleave="showDropdown" >
-                    <div class="rounded-lg shadow-2xl">
-                      <div class="rounded-lg shadow-xl">
-                        <div class="rounded-lg z-20 relative grid gap-6 px-5 sm:gap-8 sm:p-6 bg-gradient-to-br from-yellow-gold to-yellow-600" >
-                          <nuxt-link to="/login" class="-m-4 p-2 flex items-start space-x-4 rounded-lg transition ease-in-out duration-150" role="menuitem">
-                            <div class="flex-1 space-y-1">
-                              <p class="transform font-semibold hover:translate-x-2 hover:text-white leading-6 text-black text-base -mb-1 transition ease-in-out duration-150">
-                                Log In
-                              </p>
-                            </div>
-                          </nuxt-link>
-                          <nuxt-link to="/register" class="-m-4 p-2 flex items-start space-x-4 rounded-lg transition ease-in-out duration-150" role="menuitem">
-                            <div class="flex-1 space-y-1">
-                              <p class="transform font-semibold hover:translate-x-2 hover:text-white leading-6 text-black text-base -mb-1 transition ease-in-out duration-150">
-                                Register
-                              </p>
-                            </div>
-                          </nuxt-link>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </transition>
-              </div>
-              <div  v-show="$auth.loggedIn" class="relative flex justify-center text-left" @mouseleave="showDropdown">
-                <button type="button" class="capitalize inline-flex items-center mx-2  font-semibold px-1 pr-3 bg-white text-black rounded-full transform hover:scale-105 transition ease-in-out duration-100" @mouseenter="showDropdown" @:click="$router.push('/profile')">
-                  <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd" clip-rule="evenodd" d="M43.2 24C43.2 34.6039 34.6039 43.2 24 43.2C13.3962 43.2 4.80002 34.6039 4.80002 24C4.80002 13.3961 13.3962 4.79999 24 4.79999C34.6039 4.79999 43.2 13.3961 43.2 24ZM28.8 16.8C28.8 19.451 26.651 21.6 24 21.6C21.3491 21.6 19.2 19.451 19.2 16.8C19.2 14.149 21.3491 12 24 12C26.651 12 28.8 14.149 28.8 16.8ZM23.9999 26.4C19.1578 26.4 14.9855 29.2679 13.089 33.3977C15.7297 36.4609 19.6384 38.4 24 38.4C28.3615 38.4 32.2701 36.4609 34.9108 33.3979C33.0143 29.268 28.842 26.4 23.9999 26.4Z" fill="#000000"/>
-                  </svg>
-                  <span>{{ names }}</span>
-                  <svg class="-mr-1 ml-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                  </svg>
-                </button>
-                <transition
-                  enter-active-class="transition ease-out duration-200"
-                  leave-active-class="transition ease-in duration-150"
-                  enter-class="transform opacity-0 translate-y-1"
-                  enter-to-class="transform opacity-100 translate-y-0"
-                  leave-class="transform opacity-100 translate-y-0"
-                  leave-to-class="transform opacity-0 translate-y-1"
-                >
-                  <div v-show="dropDownOpen" class="absolute right-0 -mr-3 px-2 w-screen mt-16 sm:px-0 lg:ml-0 capitalize" style="max-width: 15rem;">
-                    <div class="rounded-lg shadow-2xl">
-                      <div class="rounded-lg shadow-xl">
-                        <div class="rounded-lg z-20 relative grid gap-6 px-5 sm:gap-8 sm:p-6 bg-gradient-to-br from-yellow-gold to-yellow-600" >
-                          <div class="-m-4 p-2 flex items-start space-x-4 rounded-lg transition ease-in-out duration-150" role="menuitem" @click="$router.push('/profile');showDropdown;">
-                            <div class="flex-1 space-y-1">
-                              <div class="transform cursor-pointer font-semibold hover:translate-x-2 hover:text-white leading-6 text-left text-black text-base -mb-1 transition ease-in-out duration-150">
-                                Profile
-                              </div>
-                            </div>
-                          </div>
-                          <div class="-m-4 p-2 flex items-start space-x-4 rounded-lg transition ease-in-out duration-150" role="menuitem" @click="logout">
-                            <div class="flex-1 space-y-1">
-                              <div class="transform cursor-pointer font-semibold hover:translate-x-2 hover:text-white leading-6 text-left text-black text-base -mb-1 transition ease-in-out duration-150">
-                                Log Out
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </transition>
-              </div>
             </div>
             <div class="lg:hidden mx-8 absolute z-20 right-0" >
               <button class="block lg:hidden inline-flex items-center justify-center p-2 rounded-md text-black hover:text-white focus:outline-none focus:text-white" @click="showMobileMenu = !showMobileMenu" >
